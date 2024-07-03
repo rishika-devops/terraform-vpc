8dea261222653782897bc1ffeea12f5f16012d11
sowji <jujjurisowjanya@gmail.com>
2024-07-03 12:50:02 +0530

    new

--- output.tf
+++ output.tf
@@ -1,13 +1,13 @@
 output  "azs" {
   value       = data.aws_availability_zones.azs.names
 }
-output "public_subnets_ids" {
+output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
-output "private_subnets_ids" {
+output "private_subnet_ids" {
   value = aws_subnet.private[*].id
 }
-output "database_subnets_ids" {
+output "database_subnet_ids" {
   value = aws_subnet.database[*].id
 }
 output "vpc_id" {
